c5bc937bec3aad48825c994f1a0395c3b28cd407
Matthew Schumaecker <matthew.schumaecker@gmail.com>
2024-08-24 23:32:44 -0400

    recent commit

--- lua/settings.lua
+++ lua/settings.lua
@@ -22,6 +22,8 @@ vim.opt.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.opt.showmode = false
 
+vim.opt.tabstop = 4
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
@@ -115,4 +117,3 @@ vim.api.nvim_create_autocmd('TextYankPost', {
     vim.highlight.on_yank()
   end,
 })
-
